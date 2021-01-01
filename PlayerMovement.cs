ac20df5566ebf5a30953971777ef163d1317492d
Shamima Ali <snali@mun.ca>
2021-01-01 19:20:55 -0330

    Created for the left and right movement of player

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float motionSpeed;
-    public RigidBody2D rb;
+    public Rigidbody2D rb;
     float max;
     
     // Update is called once per frame
     void Update()
     {
-      
+      max = Input.GetAxisRaw("Horizontal");
     }
+
+    private void FixedUpdate() 
+    {
+        Vector2 movement = new Vector2 (max*motionSpeed, rb.velocity.y);
+        rb.velocity = movement;
+    }
+
 }
