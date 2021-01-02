9bba66cdbc26e0e557e655c956f2777924107caf
Shamima Ali <snali@mun.ca>
2021-01-02 04:26:45 -0330

    Adding files for player shot animations

---
+++ Shot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    public float bulletSpeed = 15f;
+    public float bulletDamage = 10f;
+    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        rb.velocity = Vector2.right * bulletSpeed;
+
+    }
+
+    private void OnCollisionEnter2D (Collision2D collision )
+    {
+        Destroy(gameObject);
+    }
+}
