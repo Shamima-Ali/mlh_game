c8d0758f3945a481448b9dd70eea66724b8a6c92
Shamima Ali <snali@mun.ca>
2021-01-01 22:40:43 -0330

    Code for jump

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -7,17 +7,31 @@ public class PlayerMovement : MonoBehaviour
     public float motionSpeed;
     public Rigidbody2D rb;
     float max;
+    public float jumpforce = 20f;
     
     // Update is called once per frame
     void Update()
     {
       max = Input.GetAxisRaw("Horizontal");
+      if (Input.GetButtonDown ("Jump"))
+      {
+          Jump();
+      }
     }
 
     private void FixedUpdate() 
     {
         Vector2 movement = new Vector2 (max*motionSpeed, rb.velocity.y);
         rb.velocity = movement;
+
+        
     }
+
+    private void Jump()
+    {
+        Vector2 movementV = new Vector2 (rb.velocity.x, jumpforce);
+        rb.velocity = movementV;
+
+    } 
 
 }
